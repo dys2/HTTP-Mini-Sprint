452e9687fae02acb17740c57798171f79358d743
Dylan Scheidt <dys2@pitt.edu>
2017-09-05 14:53:33 -0700

    done

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 export const GET_FRIENDS = 'GET_FRIENDS';
-
 export const getFriends = () => {
-    // Add the code for this action
+    const promise = axios.get('http://localhost:5000/friends')
+    return {
+        type: GET_FRIENDS,
+        payload: promise
+    };
 };
